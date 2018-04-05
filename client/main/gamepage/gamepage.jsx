4a118482a4ba496f4d0af137c6cde7c0cad00f55
Will Clark <willdavidc@gmail.com>
2018-04-05 19:06:21 -0400

    Minor package updates

--- client/main/gamepage/gamepage.jsx
+++ client/main/gamepage/gamepage.jsx
@@ -1,6 +1,7 @@
 const _           = require('lodash');
 const createClass = require('create-react-class');
 const React       = require('react');
+const PIXI        = require('pixi.js');
 
 
 const GamePage = createClass({
@@ -12,17 +13,33 @@ const GamePage = createClass({
 
     getInitialState: function() {
         return {
-            context: {},
+            ctx: {},
         };
     },
 
     componentDidMount: function() {
+        this.setState({
+            ctx: this.refs['canvas'].getContext('2d'),
+        });
+    },
 
+    clickTest: function() {
+        console.log('Click!');
+        /*let ctx = this.state.ctx;
+        ctx.fillStyle = "#FF0000";
+        ctx.fillRect(0,0,150,75);*/
+
+        let type = "WebGL"
+        if(!PIXI.utils.isWebGLSupported()){
+            type = "canvas"
+        }
+
+        PIXI.utils.sayHello(type)
     },
 
     render: function() {
         return <div className='gamepage'>
-            <canvas className='gamecanvas'></canvas>
+            <canvas className='gamecanvas' ref='canvas' onClick={this.clickTest}/>
         </div>;
     }
 });
