42c0beeac6092823d1a46e794ed9bbd6d1993637
Will Clark <willdavidc@gmail.com>
2018-04-06 22:42:30 -0400

    Initialize pixi.js

--- client/main/gamepage/gamepage.jsx
+++ client/main/gamepage/gamepage.jsx
@@ -1,7 +1,8 @@
+"use strict";
 const _           = require('lodash/core');
 const createClass = require('create-react-class');
 const React       = require('react');
-let PIXI = {};
+let PIXI, app = {};
 
 const GamePage = createClass({
     getDefaultProps: function() {
@@ -15,8 +16,35 @@ const GamePage = createClass({
         };
     },
 
+
+
     componentDidMount: function() {
+        this.initializePixi();
+
+        window.addEventListener('resize', this.windowDidResize);
+    },
+
+    windowDidResize: function() {
+        const [width, height] =  [this.refs['gamepage'].clientWidth, this.refs['gamepage'].clientHeight];
+        app.renderer.autoResize = true;
+        app.renderer.resize(width, height);
+    },
+
+
+
+    initializePixi: function () {
         PIXI = require('pixi.js');
+        const [width, height] =  [this.refs['gamepage'].clientWidth, this.refs['gamepage'].clientHeight];
+
+        app = new PIXI.Application({
+            width,
+            height,
+            antialias: true,
+            transparent: true,
+            resolution: 1,
+        });
+
+        this.refs['gamepage'].appendChild(app.view);
     },
 
     clickTest: function() {
@@ -30,9 +58,10 @@ const GamePage = createClass({
         PIXI.utils.sayHello(type)
     },
 
+
+
     render: function() {
-        return <div className='gamepage'>
-            Hello!
+        return <div className='gamepage' ref='gamepage'>
         </div>;
     }
 });
